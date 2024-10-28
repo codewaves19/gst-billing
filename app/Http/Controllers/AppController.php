da018ab7cca7df112f5742f367500d4fe7a6a64c
manisha <dayalmanisha14@gmail.com>
2024-10-28 20:54:38 +0530

    Database operations using query builder method

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -3,74 +3,49 @@
 // path location of controller file ...also namespace to avoid conflicts if same name exist at different location
 namespace App\Http\Controllers; // avoid naming conflicts
 
+// Facades
 use Illuminate\Http\Request;
 use App\Models\Party;
+use Illuminate\Support\Facades\DB; // Facade is a design pattern in laravel which makes various components of application accessible and provide consistent and clean APIs to developer. they are attached to service components
 
 class AppController extends Controller
 {
     // create a function here
     public function index() {
-        //$name = "Manisha";
-        //$phone = "9810492993";
+        // using query builder
         
-
-        // usin php compact function
-        //return view("welcome", compact("name", "phone"));
-
-        // using associative array
-       /* return view("welcome", array(
-            'name' => $name,
-            'phone' => $phone
-        ));*/
-       // using associative array
-      // $data['name'] = $name;
-       //$data['phone'] = $phone;
-
-       //return view("welcome", $data);
-        // using with method
-        /*return view("welcome")
-            ->with('name', $name)
-            ->with('phone', $phone);*/
-       // Insert option 1
-        $party = new Party;
-        $party->full_name = "Money";
-        //$party->save();
-        // Insert option 2
-        $param=array(
-            "full_name" => "Roy",
-            "phone" => "5456545645",
-            "address" => "uyghgtfthgjh"
+        // insert record 
+        $param = array(
+            'full_name' => 'Rohit'
         );
-        //Party::create($param);
+        //DB::table('parties')->insert($param); // here it will ignore that we have passed value to only one field of table. it will pass null for other columns
 
-        // select all data
-        $parties= Party::all();
-        //echo $parties;
-       // dd($parties);
 
-        // select one record by id
-        //$id = 1;
-        //$party = Party::find($id);
-        //dd($party);
+        // select all
+        $party = DB::table('parties')->get();
 
-        // select record by column/field
-        //$party = Party::where('phone', '5456545645')->get();
-        //dd($party);
+        // select one record
+        $party = DB::table("parties")
+        ->where('id', 2)
+        ->where('phone', 5456545645)
+        ->get();
 
-        // update record
-        $id = 1;
-        $party = Party::find($id);
-        //$party->full_name = 'Manisha';
-        //$party->save();
-        //dd($party);
+        echo "<pre>";
+        print_r($party);
 
-        // delete record
-        $id = 1;
-        $party = Party::find($id);
-        $party->delete();
+        // update
+        $param = array(
+            'full_name' => 'Sangmitra'
+        );
+       // DB::table('parties')
+        //->where('id', 4)
+        //->update($param);
+
+        // delete
+        DB::table('parties')
+        ->where('id', 6);
 
         return "Database operations";
-
     }
         // create a function here
         public function about() {
